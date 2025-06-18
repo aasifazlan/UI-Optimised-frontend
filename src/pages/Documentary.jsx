1ea88fa4f8fac2921553f9f988826c68c0402fc3
Aasif Azlan <aasifazlaan@gmail.com>
2025-06-18 21:13:30 +0530

    DocPage

---
+++ src/pages/Documentary.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ComingSoonCard from '../components/ComingSoonCard'
+
+function Documentary() {
+  return (
+    <div className='mt-32'>
+      <ComingSoonCard/>
+    </div>
+  )
+}
+
+export default Documentary
